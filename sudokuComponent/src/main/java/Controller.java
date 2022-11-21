36196b6d4e8fb851c1dfc9b2d8dcbb0cf69c8eb9
Juan-dev123 <378tech@gmail.com>
2022-11-21 00:46:05 -0500

    constraint propagation implemented

--- sudokuComponent/src/main/java/Controller.java
+++ sudokuComponent/src/main/java/Controller.java
@@ -3,10 +3,23 @@ import java.util.ArrayList;
 public class Controller {
 
     public static void main(String[] args){
+        test();
+    }
+
+    //BORRARRRR
+    public static void test(){
         Sudoku sudo = new Sudoku();
+        String test_grid1 = "003020600900305001001806400008102900700000008006708200002609500800203009005010300";
+        String test_grid2 = "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......";
+        boolean result = sudo.parseGrid(test_grid2);
+        System.out.println(result);
+        if(result){
+            System.out.println(sudo.display());
+        }
         ArrayList<ArrayList<String>> units = sudo.getUnits().get("C2");
         ArrayList<String> peers = sudo.getPeers().get("C2");
         System.out.println("hi :)");
+
     }
 
 }
--- sudokuComponent/src/main/java/Sudoku.java
+++ sudokuComponent/src/main/java/Sudoku.java
@@ -2,15 +2,18 @@ import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
+//Adapted from http://norvig.com/sudoku.html and http://pankaj-k.net/sudoku/sudoku.js
 public class Sudoku{
 
-    private String digits = "123456789";
-    private String[] rows = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
-    private String[] cols = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-    private ArrayList<String> squares;
-    private ArrayList<ArrayList<String>> unitlist;
+    private final String digits = "123456789";
+    private final String[] rows = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+    private final String[] cols = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    private final ArrayList<String> squares;
+    private final ArrayList<ArrayList<String>> unitlist;
     private Dictionary<String, ArrayList<ArrayList<String>>> units;
     private Dictionary<String, ArrayList<String>> peers;
+
+    private Dictionary<String, String> values;
 
     public Sudoku(){
         squares = cross(rows, cols);
@@ -20,6 +23,7 @@ public class Sudoku{
         fillUnits();
         peers = new Hashtable<>();
         fillPeers();
+        values = new Hashtable<>();
     }
 
     private ArrayList<String> cross(String[] rowsP, String[] colsP){
@@ -91,6 +95,110 @@ public class Sudoku{
             }
             peers.put(s, tempPeers);
         }
+    }
+
+    public boolean parseGrid(String grid){
+        String grid2 = "";
+        for (int i = 0; i < grid.length(); i++) {
+            if("0.,-123456789".indexOf(grid.charAt(i)) >= 0){
+                grid2 += grid.charAt(i);
+            }
+        }
+        //for var s in squares QUITARRR
+        for (int i = 0; i < squares.size(); i++) {
+            //To start, every square can be any digit; then assign values from the grid.
+            values.put(squares.get(i), digits);
+        }
+        //for var s in squares QUITARRR
+        for (int i = 0; i < squares.size(); i++) {
+            if(digits.indexOf(grid2.charAt(i)) >= 0 && !assign(squares.get(i), String.valueOf(grid2.charAt(i)))){
+                return false; //Fail if we can't assign the digit to the square.
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Eliminate all the other values (except digit) from values.get(square) and propagate.
+     * @return False if a contradiction is detected
+     */
+    private boolean assign(String square, String digit){
+        boolean result = true;
+        String other_values = values.get(square).replace(digit, "");
+        for(int i = 0; i < other_values.length(); i++){
+            String tempDigit = String.valueOf(other_values.charAt(i));
+            result = result && (eliminate(square, tempDigit) ? true : false);
+        }
+        return result;
+    }
+
+    /**
+     * Eliminate digit from values.get(square), propagate when values or places <= 2
+     * @param square
+     * @param digit
+     * @return False if a contradiction is detected
+     */
+    private boolean eliminate(String square, String digit){
+        if(!values.get(square).contains(digit)){
+            return true; //The digit was already eliminated
+        }
+        String tempDigits = values.get(square).replace(digit, "");
+        values.put(square, tempDigits);
+        // If a square is reduced to one value d2, then eliminate d2 from the peers.
+        if (values.get(square).length() == 0){
+            return false; //Contradiction: removed last value
+        } else if (values.get(square).length() == 1) { // If there is only one value left in square, remove it from peers
+            boolean result = true;
+            for(int i = 0; i < peers.get(square).size(); i++){
+                result = result && (eliminate(peers.get(square).get(i), values.get(square)) ? true : false);
+            }
+            if(!result){
+                return false;
+            }
+        }
+        // If a unit is reduced to only one place for a value d, then put it there.
+        for (int u = 0; u < units.get(square).size(); u++) {
+            ArrayList<String> dplaces = new ArrayList<>();
+            for (int s = 0; s < units.get(square).get(u).size(); s++) {
+                String square2 = units.get(square).get(u).get(s);
+                if(values.get(square2).indexOf(digit) != -1){
+                    dplaces.add(square2);
+                }
+            }
+
+            if (dplaces.size() == 0){
+                return false; //Contradiction: no place for this value
+            } else if (dplaces.size() == 1) {
+                //the digit can only be in one place in unit, assign it there
+                if(!assign(dplaces.get(0), digit)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public String display(){
+        String grid = "";
+        for (int i = 0, j = 1, k = 1, l = 1; i < squares.size(); i++, j++, k++) {
+            if(l == 4){
+                grid += "------+------+------\n";
+                l = 1;
+            }
+            if(j == 3){
+                if(k == 9){
+                    grid += values.get(squares.get(i)) + "\n";
+                    k = 0;
+                    l ++;
+                }else{
+                    grid += values.get(squares.get(i)) + " |";
+                }
+                j = 0;
+            }else{
+                grid += values.get(squares.get(i)) + " ";
+            }
+        }
+        return grid;
     }
 
     private boolean isMember (String item, ArrayList<String> list){
