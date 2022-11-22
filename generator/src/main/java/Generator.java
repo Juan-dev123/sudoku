97829a7dc707a816edaee52911c334889fe239a9
Juan Esteban Caicedo Alzate <juancaicedo2000@gmail.com>
2022-11-22 18:35:36 -0500

    Sudoku grid generator done

---
+++ generator/src/main/java/Generator.java
@@ -0,0 +1,55 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class Generator {
+
+    private static final String DELIMITER = ",";
+    private static final int TOTAL_CELLS = 81;
+    private static final int ROWS_COLUMNS = 9;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //Integer.parseInt(args[0])
+        String[][] inputGrid = generate(TOTAL_CELLS - 18);
+        writeToTextFile(inputGrid);
+    }
+
+    private static String[][] generate(int emptySquares) {
+        String[][] grid = new String[ROWS_COLUMNS][ROWS_COLUMNS];
+        Random random = new Random();
+        for (String[] square : grid) {
+            for (int j = 0; j < square.length; j++)
+                square[j] = String.valueOf(random.nextInt(ROWS_COLUMNS - 1) + 1);
+        }
+        return eraseCells(grid, emptySquares);
+    }
+
+    private static String[][] eraseCells(String[][] grid, int emptySquares) {
+        Random random = new Random();
+        for (int i = 0; i < emptySquares; i++) {
+            int randomRow = random.nextInt(ROWS_COLUMNS);
+            int randomColumn = random.nextInt(ROWS_COLUMNS);
+            if (!grid[randomRow][randomColumn].isEmpty())
+                grid[randomRow][randomColumn] = "";
+            else
+                i--;
+        }
+        return grid;
+    }
+
+    private static void writeToTextFile(String[][] grid) throws FileNotFoundException {
+        //PrintWriter printWriter = new PrintWriter("resources/inputSudoku.txt");
+        for (String[] square : grid) {
+            for (int j = 0; j < square.length; j++) {
+                if (j != 8)
+                    System.out.print(square[j] + DELIMITER);
+                    //printWriter.write(strings[j] + DELIMITER);
+                else
+                    System.out.print(square[j]);
+                //printWriter.write(strings[j]);
+            }
+            System.out.println();
+            //printWriter.write("\n");
+        }
+    }
+}
